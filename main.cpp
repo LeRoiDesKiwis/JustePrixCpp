ed5ef0bfc6ac1d3d616ba517de1c015404744566
LeRoiDesKiwis <lekiwidu17@gmail.com>
2022-11-27 23:42:49 +0100

    Suppression des espaces et ajout de saut de ligne

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@ int main() {
     std::srand(std::time(nullptr));
     std::string error { "Merci de taper un nombre valide :" };
     int max {};
-    input("Valeur maximale : ", error, max);
+    input("Valeur maximale :", error, max);
 
     int value {};
     int random_value {rand()%max};
 
     while(value != random_value){
-        input("Entrez votre valeur : ", error, value);
+        input("Entrez votre valeur :", error, value);
         std::cout << test_number(value, random_value) << std::endl;
     }
 
@@ -22,9 +22,9 @@ int main() {
 }
 
 void input(const std::string& text, const std::string& error, int& var){
-    std::cout << text;
+    std::cout << text << std::endl;
     while(!(std::cin >> var)){
-        std::cout << std::endl << error;
+        std::cout << std::endl << error << std::endl;
         std::cin.clear();
         std::cin.ignore(255, '\n');
     }
