acc7cae155e4fa3594cd65ec5b6836d90091df66
LeRoiDesKiwis <lekiwidu17@gmail.com>
2022-11-27 23:27:25 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <ctime>
+
+void input(const std::string& text, const std::string& error, int& var);
+std::string test_number(int value, int random_value);
+
+int main() {
+    std::srand(std::time(nullptr));
+    std::string error { "Merci de taper un nombre valide :" };
+    int max {};
+    input("Valeur maximale : ", error, max);
+
+    int value {};
+    int random_value {rand()%max};
+
+    while(value != random_value){
+        input("Entrez votre valeur : ", error, value);
+        std::cout << test_number(value, random_value) << std::endl;
+    }
+
+    return 0;
+}
+
+void input(const std::string& text, const std::string& error, int& var){
+    std::cout << text;
+    while(!(std::cin >> var)){
+        std::cout << std::endl << error;
+        std::cin.clear();
+        std::cin.ignore(255, '\n');
+    }
+}
+
+std::string test_number(int value, int random_value){
+    if (value > random_value) return "Le nombre est trop grand.";
+    else if (value < random_value) return "Le nombre est trop petit";
+    return "Le nombre est correct.";
+}
